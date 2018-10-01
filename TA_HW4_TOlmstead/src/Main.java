707efe8b3b007eefd00fd102b57afb51c0552f46
Travis Olmstead <travis.olmstead@western.edu>
2018-09-30 22:59:23 -0600

    Started 68-70, 80-85.

--- TA_HW4_TOlmstead/src/Main.java
+++ TA_HW4_TOlmstead/src/Main.java
@@ -19,5 +19,84 @@ public class Main {
         System.out.println("Question 29: Length of the arraylist");//Check
         System.out.println("Question 31:\nindex 0 = 7\nindex 1 = 45\nindex 2 = 4\nindex 3 = 21");
         System.out.println("Question 33: sout(geo[1][2])");//Check
+
+        System.out.println("Question 68: ");
+        double val[][] = new double[10][6];
+        System.out.println("Number of Rows in array: " + numRows(val));
+        System.out.println("Question 69: ");
+        float elements[][] = new float[5][6];
+        System.out.println("Number of element = " + numElements(elements));
+        System.out.println("Question 70: ");//Ask someone about 70
+        boolean bool[][] = new boolean[2][2];
+        bool[0][0] = true;
+        bool[0][1] = true;
+        bool[1][0] = false;
+        bool[1][1] = true;
+        printBool(bool);
+        System.out.println("Number of 2-element columns: " + twoElements(bool));
+        System.out.println("Question 80: ");
+        String str[][] = new String[2][2];
+        str[0][0] = "Hello";
+        str[0][1] = "World";
+        str[1][0] = "String";
+        str[1][1] = "Output";
+        System.out.println(concatenation(str));
+        System.out.println("Question 81: ");
+
     }//main
+
+    public static int numRows(double value[][])
+    {
+        return value.length;
+    }//numRows
+    public static String concatenation(String str[][])
+    {
+        String s = "";
+        for (int i=0; i<str.length; i++)
+        {
+            for (int j=0; j<str[i].length; j++)
+            {
+                s += str[i][j] + " ";
+            }
+        }
+        return s;
+    }
+    public static int numElements(float elements[][])
+    {
+        int count = 0;
+        for (int i=0; i<elements.length; i++)
+        {
+            for (int j=0; j<elements[i].length; j++)
+            {
+                count++;
+            }//inner for loop
+        }//out for loop
+        return count;
+    }//numRows
+    public static int twoElements(boolean b[][])
+    {
+        int count = 0;
+        for (int i=0; i<b.length; i++)
+        {
+            for (int j=0; j<b[i].length; j++)
+            {
+                //System.out.print(b[i][j] + " ");
+                if(b[j].length == 2){
+                    count++;
+                }
+            }
+            //System.out.println("");
+        }
+        return count;
+    }//numElements
+    public static void printBool(boolean b[][]){
+        for (int i=0; i<b.length; i++)
+        {
+            for (int j=0; j<b[i].length; j++)
+            {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
 }//Main
